69dc53a135fcc915012e55918a9a0ece7b78eee3
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-03-27 12:54:34 +0100

    change syntax to jsx

--- components/App.js
+++ components/App.js
@@ -21,8 +21,12 @@ var contactForm = {
 }
 App = React.createClass({
     render: function() {
-        return (React.createElement('div', {className: 'app'},
-         React.createElement(ContactForm, {contact: contactForm}),
-         React.createElement(Contacts, {items: contacts}, {})))
+        return (
+          <div className='app'>
+          <ContactForm contact={contactForm}/>
+          <Contacts items={contacts}/>
+
+          </div>
+       )
     }
 })
--- components/Contacts.js
+++ components/Contacts.js
@@ -4,11 +4,16 @@
     },
   render: function() {
     var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
+        return(
+          <Contact item={contact} key={contact.id}/>)
     });
 
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
+
+      <ul className='contactsList'>
+        <li>{contacts}</li>
+      </ul>
+
     );
   }
 });
